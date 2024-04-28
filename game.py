a6009db705a9841582a36124a5fb9ead90f5b67d
David Zhao <zhadav@utschools.ca>
2024-04-28 17:48:51 -0400

    Added background logic

--- game.py
+++ game.py
@@ -20,10 +20,34 @@ pygame.display.set_caption("Platformer") #sets the title at the top of the game 
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def get_background(name): 
+    image = pygame.image.load(join("assets", "Background", name)) 
+    x, y, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height) # pos of the top-left corner of the background tile
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(window, tiles, bg_image):
+    for tile in tiles:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
 def main(window):
-
     clock = pygame.time.Clock()
-
+    tiles, bg_image = get_background("Blue.png")
+    # Blue
+    # Brown
+    # Gray
+    # Green
+    # Pink
+    # Purple
+    # Yellow
 
     run = True
     while run:
@@ -33,6 +57,9 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+    
+        
+        draw(window, tiles, bg_image)
     
     pygame.quit
     quit()
